bbbbd27f9e0e1570ac30675b87d56d10d2d7334a
youssefreda4 <youssefreda472003@gmail.com>
2025-11-30 23:34:41 +0200

    add some endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\HoldController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login');
+});
+
+Route::get('products', [ProductController::class, 'index']);
+
+
+Route::prefix('holds')->controller(HoldController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
+});
+
+Route::prefix('orders')->controller(OrderController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
 });
